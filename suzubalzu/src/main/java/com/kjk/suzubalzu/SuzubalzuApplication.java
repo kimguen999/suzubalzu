b2f33adcc6f98df963fe57ab5d7274b0838dfc24
kimguen999 <>
2026-05-17 23:07:21 +0900

    백프론트생성

---
+++ suzubalzu/src/main/java/com/kjk/suzubalzu/SuzubalzuApplication.java
@@ -0,0 +1,13 @@
+package com.kjk.suzubalzu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuzubalzuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuzubalzuApplication.class, args);
+	}
+
+}
---
+++ suzubalzu/src/test/java/com/kjk/suzubalzu/SuzubalzuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjk.suzubalzu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuzubalzuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
